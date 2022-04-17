087edc67b6c6bb0f014b4f1aba53100b60123626
UttamSaha <uttamsaha982@gmail.com>
2022-04-17 16:40:16 +0600

    services added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../Banner/Banner';
+import Services from '../Services/Services';
 
 const Home = () => {
   return (
     <div>
         <Banner></Banner>
+        <Services></Services>
     </div>
   )
 }
---
+++ src/components/Service/Service.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Service.css";
+const Service = ({ service }) => {
+  const { id, name, img, description, price } = service;
+  const navigate = useNavigate();
+  const navigateToServiceDetail = (id) => {
+    navigate(`/service/${id}`);
+  };
+  return (
+    <div className="service">
+      <img src={img} alt="" />
+      <h4 className="mt-2">{name}</h4>
+      <p>Price : {price}</p>
+      <p><small className="description">{description}</small></p>
+      <button onClick={() => navigateToServiceDetail(id)}
+        className="btn btn-dark position-relative bottom-0"
+      >
+        Book {name}
+      </button>
+    </div>
+  );
+};
+export default Service;
